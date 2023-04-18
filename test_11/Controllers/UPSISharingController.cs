fb7a2488d9be41fc8c9705e8822e96ed7dd961a8
dotnet-code <@dotnet-code>
2023-04-18 19:26:33 +0530

    commit from code202304182633

---
+++ test_11/Controllers/UPSISharingController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UPSISharingController : ControllerBase
+    {
+        // GET: api/UPSISharing
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/UPSISharing/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/UPSISharing
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/UPSISharing/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
